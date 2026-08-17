b61b18378ae65f9bb7be7df158fe242f07c33867
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2026-08-17 22:44:39 +0000

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,31 @@
+<?php
+
+$host = "localhost";
+$dbname = "hibs_reports";
+$username = "root";
+$password = "";
+
+try {
+
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $conn->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    $conn->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+
+} catch (PDOException $e) {
+
+    die("Database connection failed: " . $e->getMessage());
+
+}
+?>
